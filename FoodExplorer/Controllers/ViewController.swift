90bbd7b2e9b5b3d8bef0aa663e7b812f9da14b10
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-08-30 14:25:05 +0300

    Folders refactoring and setup project

---
+++ FoodExplorer/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+final class ViewController: UIViewController {
+    // MARK: - Constants
+    // MARK: - Properties
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // MARK: - Setups
+    // MARK: - Helpers
+    // MARK: - Actions
+    // MARK: - Objc Methods
+}
