9a323e573e4c435caf2d1e6368ceadf82235677a
Anatoli Stepanuke <anatolistepanuke@yandex.by>
2022-09-21 19:34:56 +0300

    Created custom type UIScrollView - ExploreUIScrollView() code refactoring

--- FoodExplorer/Controllers/Main/Explore Screen/ExploreViewController.swift
+++ FoodExplorer/Controllers/Main/Explore Screen/ExploreViewController.swift
@@ -19,11 +19,7 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
     private let headerLabel = ExploreUILabel(text: "Discovery new places", height: 36, fontSize: 30, fontColor: .black)
     private let footerLabel = ExploreUILabel(text: "Top categories", height: 24, fontSize: 20, fontColor: .black)
     private let searchField = ExploreUITextField()
-    private let scrollView: UIScrollView = {
-        let view = UIScrollView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    private let scrollView = ExploreUIScrollView()
     private let scrollStackViewContainer: UIStackView = {
         let view = UIStackView()
         view.axis = .vertical
@@ -31,7 +27,7 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
-    private let subView1: UIView = {
+    private let subView: UIView = {
         let view = UIView()
         view.heightAnchor.constraint(equalToConstant: 690).isActive = true
         return view
@@ -51,35 +47,35 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: - Setups
     private func setupView() {
-        let margins = view.layoutMarginsGuide
         view.addSubview(scrollView)
-
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
-        scrollView.addSubview(subView1)
-
-        scrollStackViewContainer.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        scrollStackViewContainer.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        scrollStackViewContainer.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        scrollStackViewContainer.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        scrollView.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
+            trailing: view.safeAreaLayoutGuide.trailingAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor
+        )
+        scrollView.addSubview(scrollStackViewContainer)
+        scrollStackViewContainer.anchor(
+            top: scrollView.topAnchor,
+            leading: scrollView.leadingAnchor,
+            trailing: scrollView.trailingAnchor,
+            bottom: scrollView.bottomAnchor
+        )
         scrollStackViewContainer.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
-        scrollStackViewContainer.addArrangedSubview(subView1)
-
-        subView1.addSubview(searchField)
-        subView1.addSubview(headerLabel)
-        subView1.addSubview(discoveryCollectionView)
-        subView1.addSubview(footerLabel)
-        subView1.addSubview(topCategoriesCollectionView)
+        scrollStackViewContainer.addArrangedSubview(subView)
+        subView.addSubview(searchField)
+        subView.addSubview(headerLabel)
+        subView.addSubview(discoveryCollectionView)
+        subView.addSubview(footerLabel)
+        subView.addSubview(topCategoriesCollectionView)
     }
 
     private func setupSearchField() {
         searchField.delegate = self
         searchField.anchor(
-            top: subView1.topAnchor,
-            leading: subView1.leadingAnchor,
-            trailing: subView1.trailingAnchor,
+            top: subView.topAnchor,
+            leading: subView.leadingAnchor,
+            trailing: subView.trailingAnchor,
             bottom: nil,
             padding: .init(top: 0, left: 16, bottom: 0, right: 16)
         )
@@ -88,8 +84,8 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
     private func setupHeaderLabel() {
         headerLabel.anchor(
             top: searchField.bottomAnchor,
-            leading: subView1.leadingAnchor,
-            trailing: subView1.trailingAnchor,
+            leading: subView.leadingAnchor,
+            trailing: subView.trailingAnchor,
             bottom: nil,
             padding: .init(top: 24, left: 16, bottom: 0, right: 16)
         )
@@ -98,8 +94,8 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
     private func setupDiscoveryCollectionView() {
         discoveryCollectionView.anchor(
             top: headerLabel.bottomAnchor,
-            leading: subView1.leadingAnchor,
-            trailing: subView1.trailingAnchor,
+            leading: subView.leadingAnchor,
+            trailing: subView.trailingAnchor,
             bottom: nil,
             padding: .init(top: 18, left: 0, bottom: 0, right: 0)
         )
@@ -109,8 +105,8 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
     private func setupFooterLabel() {
         footerLabel.anchor(
             top: discoveryCollectionView.bottomAnchor,
-            leading: subView1.leadingAnchor,
-            trailing: subView1.trailingAnchor,
+            leading: subView.leadingAnchor,
+            trailing: subView.trailingAnchor,
             bottom: nil,
             padding: .init(top: 24, left: 16, bottom: 0, right: 16)
         )
@@ -119,8 +115,8 @@ final class ExploreViewController: UIViewController, UITextFieldDelegate {
     private func setupTopCategoriesCollectionView() {
         topCategoriesCollectionView.anchor(
             top: footerLabel.bottomAnchor,
-            leading: subView1.leadingAnchor,
-            trailing: subView1.trailingAnchor,
+            leading: subView.leadingAnchor,
+            trailing: subView.trailingAnchor,
             bottom: nil,
             padding: .init(top: 16, left: 0, bottom: 0, right: 0)
         )
